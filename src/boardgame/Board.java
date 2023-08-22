1342ff4ee73f491380952e2bcfec0852adcc4d01
Camila Brito Pereira <camila-brito10@hotmail.com>
2023-08-22 12:19:11 -0300

    Stasting to implement Board and Piece

---
+++ src/boardgame/Board.java
@@ -0,0 +1,56 @@
+package boardgame;
+
+public class Board {
+
+
+	private int row;
+	private int column;
+	private Piece[][] pieces;
+	
+	public Board(int row, int column) {
+		super();
+		this.row = row;
+		this.column = column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/boardgame/Piece.java
@@ -0,0 +1,33 @@
+package boardgame;
+
+public class Piece {
+
+protected Position position;
+private Board board;
+ 
+public Piece(Board board) {
+	super();
+	this.board = board;
+    position = null;
+
+
+}
+
+protected Board getBoard() {
+	return board;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
